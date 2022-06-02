42e6af3c7f2a59bd011bcd01774eddf984291911
Ibrahim6231 <ibrahimkhan938@gmail.com>
2022-06-02 21:11:14 +0530

    array class work  assignment,week3Day4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,40 +1,42 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+// const myHelper = require('../util/helper')
+// const underscore = require('underscore')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
+// router.get('/test-me', function (req, res) {
+//     myHelper.printDate()
+//     myHelper.getCurrentMonth()
+//     myHelper.getCohortData()
+//     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
+//     console.log('The first element received from underscope function is '+firstElement)
+//     res.send('My first ever api!')
+// });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
+router.get('/sol1', function(req, res){
+    let arr = [1,2,3,5,6,7]  //find the misssing number
+    let n = arr[arr.length -1];
+    let sumArray = arr.reduce((acc,curr)=>{
+        acc = acc + curr;
+        return acc;
+    },0);
+    let missingNumber = (n*(n+1)/2) - sumArray
+    console.log(missingNumber)
+    res.send("The missingNumber from this array "+ arr + " is " + missingNumber)
 })
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
+router.get('/sol2', function(req, res){
+    let arr = [33,34,35,37,38]  //find the misssing number
+    let a = arr[0] - 1; let l = arr[arr.length -1];
+    let sumArray = arr.reduce((acc,curr)=>{
+        acc = acc + curr;
+        return acc;
+    },0);
+    let missingNumber = (l*(l+1)/2) - (a*(a+1)/2 )- sumArray
+    console.log(missingNumber)
+    res.send("The missingNumber from this array "+ arr + " is " + missingNumber)
 })
+
 
 
 module.exports = router;
