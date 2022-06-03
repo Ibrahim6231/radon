e38d90455f00f6f29ac1c24baca10260fb747bf0
Ibrahim6231 <ibrahimkhan938@gmail.com>
2022-06-03 20:38:04 +0530

    mongodb

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
+const {default : mongoose} = require('mongoose')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//to connect with mongoDB
+mongoose.connect("mongodb+srv://ibrahimDatabase1:8Nh3Y1Pj0ck4ubUC@cluster0.otjog5i.mongodb.net/ibrahimDatabse1?retryWrites=true&w=majority",{
+    useNewUrlParser : true
+})
+.then(()=> console.log("MongoDB is connected"))
+.catch(err => console.log(err)) 
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
