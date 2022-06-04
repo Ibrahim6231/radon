5c21caa0a7863f856a61dfee7a88d8e60dcc3042
Ibrahim6231 <ibrahimkhan938@gmail.com>
2022-06-04 10:11:54 +0530

    players

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,35 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 
-const playersArray =[
-                        {
-                            "name": "manish",
-                            "dob": "1/1/1995",
-                            "gender": "male",
-                            "city": "jalandhar",
-                            "sports": [ "swimming"]
-                        },
-                        {
-                            "name": "gopal",
-                            "dob": "1/09/1995",
-                            "gender": "male",
-                            "city": "delhi",
-                            "sports": ["soccer"]
-                        },
-                        {
-                            "name": "lokesh",
-                            "dob": "1/1/1990",
-                            "gender": "male",
-                            "city": "mumbai",
-                            "sports": ["soccer"]
-                        },
-                    ]
-    
-router.post("/players", function(req, res){
-    let Profile = req.body.player;
-    playersArray.push(Profile);
-    res.send({"players":playersArray})
-    // res.send("post working")
+const arrPlayers = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": ["swimming"]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": ["soccer"]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": ["soccer"]
+    },
+]
+
+router.post("/players", function (req, res) {
+    let Profile = req.body;
+    for(let i=0; i<arrPlayers.length; i++){
+        if(arrPlayers[i]["name"] === Profile.name){
+            return res.send("This name already exists, players details cannot be saved");
+        }
+    }
+    arrPlayers.push(Profile);
+    res.send({ "msg": arrPlayers })
 })
 
 
